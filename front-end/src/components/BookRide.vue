f7e7e22e226463eaa8c250b26464ce2ab594438a
Muizz Zafar <muizzzafar25351964@gmail.com>
2021-12-01 04:08:12 -0500

    added home option

--- front-end/src/components/BookRide.vue
+++ front-end/src/components/BookRide.vue
@@ -3,38 +3,95 @@
    <form @submit.prevent="bookRide">
         <div class="book-ride">
             <form>
-            <div class="form-group">
-                <label for="pickupLocation" class="form-label">Pickup Location</label>
-                <input type="text" class="form-control" id="pickupLocation" v-model="pickupLocation" placeholder="Enter pickup location">
-            </div>
-            <div class="form-group">
-                <label for="dropoffLocation" class="form-label">Dropoff Location</label>
-                <input type="text" class="form-control" id="dropoffLocation" v-model="dropoffLocation" placeholder="Enter dropoff location">
-            </div>
-            <div class="form-group">
-                <label for="pickupTime" class="form-label">Pickup Time</label>
-                <input type="time" class="form-control" id="pickupTime" v-model="pickupTime" placeholder="Enter pickup time">
-            </div>
-            <div class="form-group round-trip-gp">
-                <label for="roundTrip" class="form-label">Round Trip
-                    <span class="tooltiptext">By checking "Round Trip", you book a return trip that will take you from the dropoff location back to the pickup location at the selected time. </span>
-                </label>
-                <input type="checkbox" id="roundTrip" v-model="roundTrip">
-            </div>
-            <br>
+                <!-- Pickup Location -->
+               <div style="display: flex; justify-content: center; align-items: center; flex-direction: row;">
+                   <div class="form-group" style="width: 100%">
+                        <label for="pickupLocation" class="form-label">Pickup Location</label>
+                        <!-- <input type="text" class="form-control" id="pickupLocation" v-model="pickupLocation" placeholder="Enter pickup location"> -->
+                        <input v-if="homePickup" type="text" class="form-control" id="pickupLocation" v-model="pickupLocation" placeholder="Enter pickup location" disabled>
+                        <input v-else type="text" class="form-control" id="pickupLocation" v-model="pickupLocation" placeholder="Enter pickup location">
+                    </div>
+                    <div class="form-group">
+                        <label for="homePickup" class="form-label">Home</label>
+                        <input v-if="homeDropoff" type="checkbox" id="homePickup" v-model="homePickup" disabled>
+                        <input v-else type="checkbox" id="homePickup" v-model="homePickup">
+                    </div>
+                </div> 
+                <!-- Dropoff Location -->
+                <div style="display: flex; justify-content: center; align-items: center; flex-direction: row;">
+                    <div class="form-group" style="width: 100%">
+                        <label for="dropoffLocation" class="form-label">Dropoff Location</label>
+                        <input v-if="homeDropoff" type="text" class="form-control" id="dropoffLocation" v-model="dropoffLocation" placeholder="Enter dropoff location" disabled>
+                        <input v-else type="text" class="form-control" id="dropoffLocation" v-model="dropoffLocation" placeholder="Enter dropoff location">
+                    </div>
+                    <!-- checkbox for home -->
+                    <div class="form-group">
+                        <label for="homeDropoff" class="form-label">Home</label>
+                        <input v-if="homePickup" type="checkbox" id="homeDropoff" v-model="homeDropoff" disabled>
+                        <input v-else type="checkbox" id="homeDropoff" v-model="homeDropoff">
+                    </div>
+                </div>
+                <!-- Pickup time -->
+                <div class="form-group">
+                    <label for="pickupTime" class="form-label">Pickup Time</label>
+                    <input type="time" class="form-control" id="pickupTime" v-model="pickupTime" placeholder="Enter pickup time">
+                </div>
+                <!-- Round trip -->
+                <div class="form-group round-trip-gp">
+                    <label for="roundTrip" class="form-label">Round Trip
+                        <span class="tooltiptext">By checking "Round Trip", you book a return trip that will take you from the dropoff location back to the pickup location at the selected time. </span>
+                    </label>
+                    <input type="checkbox" id="roundTrip" v-model="roundTrip">
+                </div>
+                <br>
+                <!-- Round trip pickup time -->
+                <div v-if="roundTrip" class="form-group">
+                    <label style="display: flex; justify-content: center;" for="pickupTimeReturnTrip" class="form-label">Return Trip Pickup Time</label>
+                    <input type="time" class="form-control" id="pickupTimeReturnTrip" v-model="pickupTimeReturnTrip" placeholder="Enter pickup time">
+                </div>
+                <br>
             </form>
         </div>
+        <!-- Second Trip -->
         <p style="font-size: 30px; margin-top: 45px; font-weight: bold;">Book a Second Trip</p>
         <div class="book-ride">
             <form>
-            <div class="form-group">
+                <!-- Pickup Location -->
+                <div style="display: flex; justify-content: center; align-items: center; flex-direction: row;">
+                   <div class="form-group" style="width: 100%">
+                        <label for="pickupLocation2" class="form-label">Pickup Location</label>
+                        <!-- <input type="text" class="form-control" id="pickupLocation" v-model="pickupLocation" placeholder="Enter pickup location"> -->
+                        <input v-if="homePickup2" type="text" class="form-control" id="pickupLocation2" v-model="pickupLocation2" placeholder="Enter pickup location" disabled>
+                        <input v-else type="text" class="form-control" id="pickupLocation2" v-model="pickupLocation2" placeholder="Enter pickup location">
+                    </div>
+                    <div class="form-group">
+                        <label for="homePickup2" class="form-label">Home</label>
+                        <input v-if="homeDropoff2" type="checkbox" id="homePickup2" v-model="homePickup2" disabled>
+                        <input v-else type="checkbox" id="homePickup2" v-model="homePickup2">
+                    </div>
+                </div> 
+                <!-- Dropoff Location -->
+                <div style="display: flex; justify-content: center; align-items: center; flex-direction: row;">
+                    <div class="form-group" style="width: 100%">
+                        <label for="dropoffLocation2" class="form-label">Dropoff Location</label>
+                        <input v-if="homeDropoff2" type="text" class="form-control" id="dropoffLocation2" v-model="dropoffLocation2" placeholder="Enter dropoff location" disabled>
+                        <input v-else type="text" class="form-control" id="dropoffLocation2" v-model="dropoffLocation2" placeholder="Enter dropoff location">
+                    </div>
+                    <!-- checkbox for home -->
+                    <div class="form-group">
+                        <label for="homeDropoff2" class="form-label">Home</label>
+                        <input v-if="homePickup2" type="checkbox" id="homeDropoff2" v-model="homeDropoff2" disabled>
+                        <input v-else type="checkbox" id="homeDropoff2" v-model="homeDropoff2">
+                    </div>
+                </div>
+            <!-- <div class="form-group">
                 <label for="pickupLocation2" class="form-label">Pickup Location</label>
                 <input type="text" class="form-control" id="pickupLocation2" v-model="pickupLocation2" placeholder="Enter pickup location">
             </div>
                 <div class="form-group">
                     <label for="dropoffLocation" class="form-label">Dropoff Location</label>
                     <input type="text" class="form-control" id="dropoffLocation2" v-model="dropoffLocation2" placeholder="Enter dropoff location">
-                </div>
+                </div> -->
             <div class="form-group">
                 <label for="pickupTime" class="form-label">Pickup Time</label>
                 <input type="time" class="form-control" id="pickupTime2" v-model="pickupTime2" placeholder="Enter pickup time">
@@ -44,6 +101,11 @@
                     <span class="tooltiptext">By checking "Round Trip", you book a return trip that will take you from the dropoff location back to the pickup location at the selected time. </span>
                 </label>
                 <input type="checkbox" id="roundTrip2" v-model="roundTrip2">
+            </div>
+            <br>
+            <div v-if="roundTrip2" class="form-group">
+                <label style="display: flex; justify-content: center;" for="pickupTimeReturnTrip" class="form-label">Return Trip Pickup Time</label>
+                <input type="time" class="form-control" id="pickupTimeReturnTrip" v-model="pickupTimeReturnTrip" placeholder="Enter pickup time">
             </div>
             <br>
             </form>
@@ -64,10 +126,16 @@ export default {
             dropoffLocation: '',
             pickupTime: '',
             roundTrip: false,
+            roundTripPickUpTime: '',
+            homePickup: false,
+            homeDropoff: false,
             pickupLocation2: '',
             dropoffLocation2: '',
             pickupTime2: '',
-            roundTrip2: false
+            roundTrip2: false,
+            roundTripPickUpTime2: '',
+            homePickup2: false,
+            homeDropoff2: false,
         }
     },
     methods: {
@@ -82,10 +150,16 @@ export default {
                 dropoffLocation: this.dropoffLocation,
                 pickupTime: this.pickupTime,
                 roundTrip: this.roundTrip,
+                roundTripPickUpTime: this.roundTripPickUpTime,
+                homePickup: this.homePickup,
+                homeDropoff: this.homeDropoff,
                 pickupLocation2: this.pickupLocation2,
                 dropoffLocation2: this.dropoffLocation2,
                 pickupTime2: this.pickupTime2,
-                roundTrip2: this.roundTrip2
+                roundTrip2: this.roundTrip2,
+                roundTripPickUpTime2: this.roundTripPickUpTime2,
+                homePickup2: this.homePickup2,
+                homeDropoff2: this.homeDropoff2,
             })
             .then(response => {
                 console.log(response);
